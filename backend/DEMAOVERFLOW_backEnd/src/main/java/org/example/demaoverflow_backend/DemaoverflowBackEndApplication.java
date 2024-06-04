f9301c51a9affa4a9005966e7cc4c86746a9e35c
오송주 <dhthdwn7920@gmail.com>
2024-06-04 13:47:38 +0900

    setting:: backEnd setting

---
+++ backend/DEMAOVERFLOW_backEnd/src/main/java/org/example/demaoverflow_backend/DemaoverflowBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.example.demaoverflow_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemaoverflowBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemaoverflowBackEndApplication.class, args);
+    }
+
+}
---
+++ backend/DEMAOVERFLOW_backEnd/src/test/java/org/example/demaoverflow_backend/DemaoverflowBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demaoverflow_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemaoverflowBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
